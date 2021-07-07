254f4db82c2fa22d6ddcb52412f2e3097403288b
rupalidashore <rupali.dashore@gmail.com>
2021-07-06 23:23:11 -0700

    added features

--- src/app/grocery/grocery.component.ts
+++ src/app/grocery/grocery.component.ts
@@ -7,15 +7,49 @@ import { Component, OnInit } from '@angular/core';
               '../../assets/css/bootstrap.min.css']
 })
 export class GroceryComponent implements OnInit {
-task:string='';
-tasks: any[]=[];
-  constructor() { }
+task ={name:'',
+        id:0};
+ 
+tasks:any[]=[];
+  constructor() {     
+  }
 
   ngOnInit(): void {
   }
-  onClick(){
-    this.tasks.push({name: this.task});
-  this.task = '';
+  onClick(){   
+    this.tasks.push({id: (new Date()).getTime(),name: this.task.name});    
+    this.task.name="";
+  }
+   
+onEdit(item:any){
+  this.task=item
+  // for( var i=0;i<this.tasks.length;i++){
+  //   if(item.id==this.tasks[i].id){
+  //     this.tasks[i].name=this.task;
+  //     break;
+  //   }
+  // }
 }
 
+onDelete(item:any){
+  for( var i=0;i<this.tasks.length;i++){
+    if(item.id==this.tasks[i].id){
+      this.tasks.splice(i,1);
+      break;
+    }
+  }
+}
+onStrike(item:any){
+  for(var i = 0;i < this.tasks.length; i++){
+    if(item.id == this.tasks[i].id){
+      if(this.tasks[i].strike){
+        this.tasks[i].strike = false;
+      }
+      else{
+        this.tasks[i].strike = true;
+      }
+      break;
+    }
+  }
+}
 }
